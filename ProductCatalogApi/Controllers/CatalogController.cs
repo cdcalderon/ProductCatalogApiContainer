497cf898e40eb752d2e395b0bba8cdee63814928
SFS <cacalder@cisco.com>
2018-07-28 16:02:50 -0500

    Add Catalog Controller, configure CatalogSettings

---
+++ ProductCatalogApi/Controllers/CatalogController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ProductCatalogApi.Data;
+
+namespace ProductCatalogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private readonly CatalogContext _catalogContext;
+        private readonly IOptionsSnapshot<CatalogSettings> _settings;
+        public CatalogController(CatalogContext catalogContext, IOptionsSnapshot<CatalogSettings> settings)
+        {
+            _catalogContext = catalogContext;
+            _settings = settings;
+            ((DbContext)catalogContext).ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> CatalogTypes()
+        {
+            var items = await _catalogContext.CatalogTypes.ToListAsync();
+            return Ok(items);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> CatalogBrands()
+        {
+            var items = await _catalogContext.CatalogBrands.ToListAsync();
+            return Ok(items);
+        }
+
+    }
+}
--- ProductCatalogApi/Startup.cs
+++ ProductCatalogApi/Startup.cs
@@ -26,6 +26,7 @@ namespace ProductCatalogApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CatalogSettings>(Configuration);
             services.AddDbContext<CatalogContext>(options => options.UseSqlServer(Configuration["ConnectionString"]));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
