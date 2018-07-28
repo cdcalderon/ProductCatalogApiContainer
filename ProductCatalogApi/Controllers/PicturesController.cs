e70589a1be11f5f09687dac4b39dde9ba6846839
SFS <cacalder@cisco.com>
2018-07-28 13:52:35 -0500

    Add  DB Seed  and pictures controller

---
+++ ProductCatalogApi/Controllers/PicturesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProductCatalogApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PicturesController : ControllerBase
+    {
+        private readonly IHostingEnvironment _env;
+        public PicturesController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetImage(int id)
+        {
+            var webRoot = _env.WebRootPath;
+            var path = Path.Combine($"{webRoot}/Images/", $"stock{id}.jpg");
+            var buffer = System.IO.File.ReadAllBytes(path);
+            return File(buffer, "image/jpg");
+        }
+    }
+}
---
+++ ProductCatalogApi/Data/CatalogSeed.cs
@@ -0,0 +1,62 @@
+﻿using ProductCatalogApi.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogApi.Data
+{
+    public class CatalogSeed
+    {
+        public static async Task SeedAsync(CatalogContext context)
+        {
+            if (!context.CatalogBrands.Any())
+            {
+                context.CatalogBrands.AddRange(GetSeededCatalogBrands());
+                await context.SaveChangesAsync();
+            }
+            if (!context.CatalogTypes.Any())
+            {
+                context.CatalogTypes.AddRange(GetSeededCatalogTypes());
+                await context.SaveChangesAsync();
+            }
+            if (!context.CatalogItems.Any())
+            {
+                context.CatalogItems.AddRange(GetSeededCatalogItems());
+                await context.SaveChangesAsync();
+            }
+
+        }
+
+        static IEnumerable<CatalogBrand> GetSeededCatalogBrands()
+        {
+            return new List<CatalogBrand>()
+            {
+                new CatalogBrand(){ Brand = "Brand 1" },
+                new CatalogBrand(){ Brand = "Brand 2" },
+                new CatalogBrand(){ Brand = "Brand 3" }
+            };
+        }
+        static IEnumerable<CatalogType> GetSeededCatalogTypes()
+        {
+            return new List<CatalogType>()
+            {
+                new CatalogType(){ Type = "Type 1" },
+                new CatalogType(){ Type = "Type 2" },
+                new CatalogType(){ Type = "Type 3" }
+            };
+        }
+        static IEnumerable<CatalogItem> GetSeededCatalogItems()
+        {
+            return new List<CatalogItem>()
+            {
+                new CatalogItem(){CatalogTypeId = 2, CatalogBrandId = 3, Description = "Description", Name = "Name", Price = 333, PictureUrl = "testUrl" },
+                new CatalogItem(){CatalogTypeId = 2, CatalogBrandId = 3, Description = "Description", Name = "Name", Price = 333, PictureUrl = "testUrl" },
+                new CatalogItem(){CatalogTypeId = 2, CatalogBrandId = 3, Description = "Description", Name = "Name", Price = 333, PictureUrl = "Test url" },
+                new CatalogItem(){CatalogTypeId = 2, CatalogBrandId = 3, Description = "Description", Name = "Name", Price = 333, PictureUrl = "Test url" },
+                new CatalogItem(){CatalogTypeId = 2, CatalogBrandId = 3, Description = "Description", Name = "Name", Price = 333, PictureUrl = "Test url" },
+                new CatalogItem(){CatalogTypeId = 2, CatalogBrandId = 3, Description = "Description", Name = "Name", Price = 333, PictureUrl = "Test url" },
+            };
+        }
+    }
+}
