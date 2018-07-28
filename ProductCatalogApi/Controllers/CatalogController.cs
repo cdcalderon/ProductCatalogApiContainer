eafd2b118f91b4c08136c3244fbfe3178a919e25
SFS <cacalder@cisco.com>
2018-07-28 17:29:45 -0500

    Add Catalog items endpoints

--- ProductCatalogApi/Controllers/CatalogController.cs
+++ ProductCatalogApi/Controllers/CatalogController.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using ProductCatalogApi.Data;
+using ProductCatalogApi.Domain;
+using ProductCatalogApi.ViewModels;
 
 namespace ProductCatalogApi.Controllers
 {
@@ -39,5 +41,81 @@ namespace ProductCatalogApi.Controllers
             return Ok(items);
         }
 
+        [HttpGet]
+        [Route("items/{id:int}")]
+        public async Task<IActionResult> GetItemById(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var item = await _catalogContext.CatalogItems.SingleOrDefaultAsync(c => c.Id == id);
+            if (item != null)
+            {
+                return Ok(item);
+            }
+            return NotFound();
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> Items([FromQuery] int pageSize = 6, [FromQuery] int pageIndex = 0)
+        {
+            var totalItems = await _catalogContext.CatalogItems
+                                .LongCountAsync();
+            var itemsOnPage = await _catalogContext.CatalogItems
+                                .OrderBy(c => c.Name)
+                                .Skip(pageSize * pageIndex)
+                                .Take(pageSize)
+                                .ToListAsync();
+            var model = new PaginatedItemsViewModel<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnPage);
+            return Ok(model);
+        }
+
+        //GET api/Catalog/items/withname/StockPredictionDaily?pageSize=2&pageIndex=0
+        [HttpGet]
+        [Route("[action]/withname/{name:minlength(1)}")]
+        public async Task<IActionResult> Items(string name, [FromQuery] int pageSize = 6, [FromQuery] int pageIndex = 0)
+        {
+            var totalItems = await _catalogContext.CatalogItems
+                                .Where(c => c.Name.StartsWith(name))
+                                .LongCountAsync();
+            var itemsOnPage = await _catalogContext.CatalogItems
+                                .Where(c => c.Name.StartsWith(name))
+                                .OrderBy(c => c.Name)
+                                .Skip(pageSize * pageIndex)
+                                .Take(pageSize)
+                                .ToListAsync();
+            var model = new PaginatedItemsViewModel<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnPage);
+            return Ok(model);
+        }
+
+        //GET api/Catalog/items/type/1/brand/null[?pageSize=2&pageIndex=0]
+        [HttpGet]
+        [Route("[action]/type/{catalogTypeId}/brand/{catalogBrandId}")]
+        public async Task<IActionResult> Items(int? catalogTypeId, int? catalogBrandId, [FromQuery] int pageSize = 6, [FromQuery] int pageIndex = 0)
+        {
+            var root =(IQueryable<CatalogItem>) _catalogContext.CatalogItems;
+            if (catalogTypeId.HasValue)
+            {
+                root = root.Where(c => c.CatalogTypeId == catalogTypeId);
+            }
+
+            if (catalogBrandId.HasValue)
+            {
+                root = root.Where(c => c.CatalogBrandId == catalogBrandId);
+            }
+
+            var totalItems = await root
+                                .LongCountAsync();
+            var itemsOnPage = await root
+                                .OrderBy(c => c.Name)
+                                .Skip(pageSize * pageIndex)
+                                .Take(pageSize)
+                                .ToListAsync();
+            var model = new PaginatedItemsViewModel<CatalogItem>(pageIndex, pageSize, totalItems, itemsOnPage);
+            return Ok(model);
+        }
     }
 }
---
+++ ProductCatalogApi/ViewModels/PaginatedItemsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductCatalogApi.ViewModels
+{
+    public class PaginatedItemsViewModel<TEntity> where TEntity: class
+    {
+        public PaginatedItemsViewModel(int pageIndex, int pageSize, long count, IEnumerable<TEntity> data)
+        {
+            this.PageIndex = pageIndex;
+            this.PageSize = pageSize;
+            this.Count = count;
+            this.Data = data;
+        }
+
+        public int PageSize { get; set; }
+        public int PageIndex { get; set; }
+        public long Count { get; set; }
+        public IEnumerable<TEntity> Data { get; set; }
+
+    }
+}
