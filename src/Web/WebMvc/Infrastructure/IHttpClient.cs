e32c7a322ab441b2406087eaf5d6f37f81b76394
SFS <carlos.calderon@plexusworldwide.com>
2018-08-19 22:12:28 -0500

    Add HttpClient Infrastructure

---
+++ src/Web/WebMvc/Infrastructure/IHttpClient.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace InvestipsProductsContainer.WebMvc.Infrastructure
+{
+    public interface IHttpClient
+    {
+        Task<string> GetStringAsync(string uri);
+
+        Task<HttpResponseMessage> PostAsync<T>(string uri, T item);
+
+        Task<HttpResponseMessage> DeleteAsync(string uri);
+
+        Task<HttpResponseMessage> PutAsync<T>(string uri, T item);
+    }
+}
