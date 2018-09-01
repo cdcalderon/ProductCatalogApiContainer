bd9ba0a716cd877f69580742c0f48f8dc7c54c83
SFS <cdcalderon@gmail.com>
2018-09-01 16:52:39 -0500

    Add Domain Exchange , MutualFund, Security, Stock entities

---
+++ src/Services/SecuritiesApi/Domain/MutualFund.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecuritiesApi.Domain
+{
+    public class MutualFund : Security
+    {
+        public int MorningStarRating { get; set; }
+    }
+}
