1faab8bdd0bceda3cb1175d63da741db34850119
SFS <cdcalderon@gmail.com>
2018-09-02 01:43:25 -0500

    Add Securities Controller and Seeder method

---
+++ src/Services/SecuritiesApi/Controllers/SecurityController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using SecuritiesApi.Data;
+
+namespace SecuritiesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecurityController : ControllerBase
+    {
+        private readonly FinanceSecurityContext _securityContext;
+        private readonly IOptionsSnapshot<FinanceSecuritiesSettings> _settings;
+        public SecurityController(FinanceSecurityContext catalogContext, IOptionsSnapshot<FinanceSecuritiesSettings> settings)
+        {
+            _securityContext = catalogContext;
+            _settings = settings;
+            ((DbContext)catalogContext).ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> Stocks()
+        {
+            var items = await _securityContext.Stocks.ToListAsync();
+            return Ok(items);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> MutuaFunds()
+        {
+            var items = await _securityContext.MutualFunds.ToListAsync();
+            return Ok(items);
+        }
+    }
+}
---
+++ src/Services/SecuritiesApi/Data/SecuritiesSeed.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SecuritiesApi.Domain;
+
+namespace SecuritiesApi.Data
+{
+    public class SecuritiesSeed
+    {
+        public static async Task SeedAsync(FinanceSecurityContext context)
+        {
+            context.Database.Migrate();
+            if (!context.Exchanges.Any())
+            {
+                context.Exchanges.AddRange(GetSeededExchanges());
+                await context.SaveChangesAsync();
+            }
+            if (!context.Securities.Any())
+            {
+                context.Securities.AddRange(GetSeededSecurities());
+                await context.SaveChangesAsync();
+            }
+
+        }
+
+        static IEnumerable<Exchange> GetSeededExchanges()
+        {
+            return new List<Exchange>()
+            {
+                new Exchange(){ Title = "Nasdaq" },
+                new Exchange(){ Title = "NYSE" },
+                new Exchange(){ Title = "S&P500" }
+            };
+        }
+        static IEnumerable<Security> GetSeededSecurities()
+        {
+            return new List<Security>()
+            {
+                new Stock(){ Symbol = "AAPL", Change = 5.5m, Last = 100m, Company = "Apple Inc", DayHigh = 102m, DayLow = 99m, ExchangeId = 1 },
+                new MutualFund(){ Symbol = "MGF",MorningStarRating = 2}
+            };
+        }
+    }
+}
